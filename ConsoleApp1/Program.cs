5fceb2a47b22e0b7ef18114f29c51897600c435a
Tony Collins <tjc@eltham-college.org.uk>
2019-11-08 12:30:20 +0000

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            Student s1;
+            s1 = new Student("Benan", 16, 1.80);
+            Student s2;
+            s2= new Student("Nathan", 16, 1.7);
+
+            s1.educate();
+            s1.educate();
+
+            for(int x=0; x<365; x++)
+            {
+                s2.lateNight();
+
+            }
+            
+            s1.print();
+
+            s2.print();
+
+            s2 = null;
+      
+        }
+    }
+
+   
+}
---
+++ ConsoleApp1/Student.cs
@@ -0,0 +1,70 @@
+﻿namespace ConsoleApp1
+{
+    using System;
+    class Student
+    {
+        private int age;
+        private int IQ;
+        private string name;
+        private double height;
+
+
+
+
+
+
+        public Student(string n, int age, double height)
+        {
+            IQ = 100;
+            name = n;
+            this.age = age;
+            this.height = height;
+
+        }
+
+        public Student(string n)
+        {
+            name = n;
+            age = 11;
+            height = 1.1;
+
+        }
+
+
+
+        public double getHeight()
+        {
+            return height;
+        }
+
+
+        public void setHeight(double h)
+        {
+            height = h;
+
+        }
+
+
+        public void educate()
+        {
+            IQ = IQ + 10;
+        }
+
+        public void lateNight()
+        {
+            if (IQ > 5)
+            {
+                IQ = IQ - 5;
+            }
+        }
+
+        public void print()
+        {
+            Console.WriteLine(name);
+            Console.WriteLine(height);
+            Console.WriteLine(age);
+            Console.WriteLine(IQ);
+
+        }
+    }
+}
